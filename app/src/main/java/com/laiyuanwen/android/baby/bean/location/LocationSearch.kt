b3a4d0836e1d01c288712d57e8e4564089829c57
laiyuanwen <helloworld.wen@gmail.com>
2020-03-22 03:34:39 +0800

    ✨城市选择页Adapter✨

---
+++ app/src/main/java/com/laiyuanwen/android/baby/bean/location/LocationSearch.kt
@@ -0,0 +1,14 @@
+package com.laiyuanwen.android.baby.bean.location
+
+/**
+ * Created by laiyuanwen on 2020/3/22.
+ */
+data class LocationSearch(
+        val id: String,
+        val name: String, // 唐家人才公寓
+        val district: String, // 广东省珠海市香洲区
+        val adcode: String, // 440402
+        val address: String, // 唐淇路1083号
+        val location: String, // 113.598103,22.352248
+        val typecode: String // 120302
+)
