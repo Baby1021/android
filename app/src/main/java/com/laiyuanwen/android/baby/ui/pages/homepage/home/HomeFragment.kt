98302e6ed90113f67583f602d14074fbb2ad1971
laiyuanwen <helloworld.wen@gmail.com>
2020-07-22 00:28:06 +0800

    上传图片OSS调用

--- app/src/main/java/com/laiyuanwen/android/baby/ui/pages/homepage/home/HomeFragment.kt
+++ app/src/main/java/com/laiyuanwen/android/baby/ui/pages/homepage/home/HomeFragment.kt
@@ -1,7 +1,10 @@
 package com.laiyuanwen.android.baby.ui.pages.homepage.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,14 +12,11 @@ import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.alibaba.sdk.android.oss.ClientConfiguration
-import com.alibaba.sdk.android.oss.OSSClient
-import com.alibaba.sdk.android.oss.common.auth.OSSAuthCredentialsProvider
-import com.alibaba.sdk.android.oss.common.auth.OSSCredentialProvider
+import com.alibaba.sdk.android.oss.ClientException
+import com.alibaba.sdk.android.oss.ServiceException
+import com.alibaba.sdk.android.oss.model.PutObjectRequest
+import com.alibaba.sdk.android.oss.model.PutObjectResult
 import com.amap.api.location.AMapLocationListener
-import com.bumptech.glide.Glide
-import com.laiyuanwen.android.baby.BabyApplication.Companion.getApplicationContext
-import com.laiyuanwen.android.baby.GlideApp
 import com.laiyuanwen.android.baby.R
 import com.laiyuanwen.android.baby.base.BaseFragment
 import com.laiyuanwen.android.baby.bean.response.HomeInfo
@@ -28,8 +28,6 @@ import com.laiyuanwen.android.baby.util.setStatusBarColor
 import com.laiyuanwen.android.baby.util.toAnniversary
 import com.laiyuanwen.android.baby.util.toBill
 import com.laiyuanwen.android.baby.util.toMap
-import com.laiyuanwen.android.glide.loader.OSSImageData
-import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.coroutines.*
 
 
@@ -81,14 +79,19 @@ class HomeFragment : BaseFragment() {
             context?.let { toMap(it) }
         }
         binding.leftStateLayout.setOnClickListener {
-            toast("留言功能开发中")
-            GlobalScope.launch {
-                withContext(Dispatchers.Main) {
-                    GlideApp.with(lover_image)
-                            .load(OSSImageData(BabyOSSClient.oss, "https://image-baby.oss-cn-shenzhen.aliyuncs.com/b79689cd75cf5d3-200x180.jpg"))
-                            .into(lover_image)
-                }
-            }
+//            toast("留言功能开发中")
+//            GlobalScope.launch {
+
+            val intent = Intent(Intent.ACTION_PICK);
+            intent.type = "image/*";
+            startActivityForResult(intent, 123);
+
+//                withContext(Dispatchers.Main) {
+//                    GlideApp.with(lover_image)
+//                            .load(OSSImageData(BabyOSSClient.oss, "https://image-baby.oss-cn-shenzhen.aliyuncs.com/b79689cd75cf5d3-200x180.jpg"))
+//                            .into(lover_image)
+//                }
+//            }
         }
         binding.loverImage.setOnClickListener {
             toast("图片功能开发中")
@@ -148,4 +151,43 @@ class HomeFragment : BaseFragment() {
         viewModel.stop()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        println(requestCode)
+
+        if (data == null)
+            return
+
+        GlobalScope.launch {
+
+            val column = arrayOf<String>(MediaStore.Images.Media.DATA);
+
+            val cursor = activity?.contentResolver?.query(data.data!!, column, null, null, null)
+            cursor!!.moveToFirst()
+            val columnIndex = cursor!!.getColumnIndex(column.get(0))
+
+            val picturePath = cursor.getString(columnIndex)
+
+            Log.d("PickPicture", picturePath);
+            cursor.close();
+
+
+            val put: PutObjectRequest? = PutObjectRequest("image-baby", "test/demo.jpg", picturePath)
+            try {
+                val putResult: PutObjectResult = BabyOSSClient.oss.putObject(put)
+                Log.d("PutObject", "UploadSuccess")
+                Log.d("ETag", putResult.getETag())
+                Log.d("RequestId", putResult.getRequestId())
+            } catch (e: ClientException) {
+                // 本地异常，如网络异常等。
+                e.printStackTrace()
+            } catch (e: ServiceException) {
+                // 服务异常。
+                Log.e("RequestId", e.getRequestId())
+                Log.e("ErrorCode", e.getErrorCode())
+                Log.e("HostId", e.getHostId())
+                Log.e("RawMessage", e.getRawMessage())
+            }
+        }
+    }
 }
